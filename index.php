a048acc57059a010e3357a8e3975a14c017dd24b
Messner Daniel (Student Com19) <daniel.messner@stud-inf.unibz.it>
2020-05-13 15:30:47 +0200

    added index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Soccer team manager</title>
+  <meta name="description" content="Soccer team manager">
+  <meta name="author" content="Daniel Messner, Manuel Messner">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+  <h1>Soccer team manager</h1>
+</body>
+</html>
