93b163e924efba03750777567833e4687e7cd1f6
Messner Daniel (Student Com19) <daniel.messner@stud-inf.unibz.it>
2020-05-16 14:20:27 +0200

    style.css hinzugefügt

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
   <meta name="description" content="Soccer team manager">
   <meta name="author" content="Daniel Messner, Manuel Messner">
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="style.css">
 
 </head>
 
 <body>
   <h1>My team manager</h1>
-  <div style="width: 20%; height: 100%; background-color: green"></div>
+  <div id="aside"></div>
 </body>
 </html>
